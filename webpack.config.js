434a41b691ea58a09b82a3caa050af5b8a97d5fb
wbs <1134954328@qq.com>
2021-07-19 10:42:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -5,5 +6,19 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "wbs",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
